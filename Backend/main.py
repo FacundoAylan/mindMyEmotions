375ecb0880b80bae582502998e0e5f8a9efe6e06
Brian Alexis Paredes Andrade <68014465+bparedes21@users.noreply.github.com>
2023-03-01 01:51:51 -0300

    Logins (#30)
    * Login-
    
    * Login-
    
    * Log
    
    * Update vercel.json
    
    * Update main.py
    
    * Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-from api.index import *
-=======
 from api.index import app
 
-import uvicorn
 
 
-if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
->>>>>>> 5c3ddea662d964a29808d4aaf5a6f4315557a06b
