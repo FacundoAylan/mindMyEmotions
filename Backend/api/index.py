60689eb09ccb4565d5693d974cebda3e373419d1
Brian Alexis Paredes Andrade <68014465+bparedes21@users.noreply.github.com>
2023-03-02 22:48:56 -0300

    Rama deploy backend (#46)
    * registro2
    
    * registro2

--- Backend/api/index.py
+++ Backend/api/index.py
@@ -1,22 +1,29 @@
-#from typing import Union
 #librerias para que funcione la API
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 import json
-#librerias para que funcione la firebase
+#librerias para que funcione firebase
 import firebase_admin
 from firebase_admin import credentials, firestore,firestore_async
 from datetime import datetime
 
 class Buscar_usuario_contrasenia_class(BaseModel):
-    mail: str
-    contrasenia: str  
+    Mail: str
+    Contrasenia: str  
+
+class Registrar_Datos_Usuario_class(BaseModel):
+    Mail:str
+    Edad: int
+    Nombre_de_usuario: str
+    Apellido_de_usuario: str
+    Contrasenia:str
+    Genero: str
+    Ciudad: str
 
 cred = credentials.Certificate("serviceAccountKey.json")
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
-
 app = FastAPI()
 
 @app.get("/")
@@ -24,56 +31,51 @@ async def root():
     
     return {"Presentacion": "Api desarrollada para MyEmotion"}
 
-
-@app.post("/usuario_contrasenia1/")
+#pide dos datos usuario y contraseña
+#Busca usuario y contraseña
+#devuelve un mensaje segun el resultado de la busqueda.
+@app.post("/usuario_contrasenia/")
 def Buscar_usuario_contrasenia(user_date:Buscar_usuario_contrasenia_class):
-    #inicio en falso la variable que voy a retornar
-    Mensaje="if no funciona"
-    #conecto a la base de datos pidiendo que busque el id de usuario: el mail
-    usuario_doc=db.collection('usuarios').where("mail_y_contrasenia","array_contains",user_date.mail).get()
+    Mensaje = "No coinciden"
+    usuario_doc=db.collection('usuarios').where("Datos_registro.Mail","==",user_date.Mail).get()
     
-    if not usuario_doc:
+    if usuario_doc==[]:
         Mensaje="Usuario y/ contraseña no coinciden"
     else:
-            
         for clave in usuario_doc:
             key=clave.id
         usuario_doc_completo=db.collection('usuarios').document(key).get()
         usuario_diccionario=usuario_doc_completo.to_dict()
-        if usuario_diccionario['mail_y_contrasenia']== user_date.contrasenia :
-
+        
+        consultar_datos=usuario_diccionario['Datos_registro']
+        
+        clave=consultar_datos['Contrasenia']
+    
+        if consultar_datos['Contrasenia']== user_date.Contrasenia:
             Mensaje="Usuario y/ contraseña ingresados correctamente"
         else:
             Mensaje="Contraseña ingresada incorrectamente"
-       #inicio en falso la variable que voy a retornar
-
     return {"Mensaje":Mensaje}
-"""
-
-@app.post("/Buscar_usuario_contraseña/")
-def Buscar_usuario_contrasenia(user_date:Buscar_usuario_contrasenia_class):
-    #inicio en falso la variable que voy a retornar
-    Mensaje="if no funciona"
-    #conecto a la base de datos pidiendo que busque el id de usuario: el mail
-    usuario_doc=db.collection('usuarios').where("mail","==",user_date.mail).get()
     
-    if not usuario_doc:
-        Mensaje="Usuario y/ contraseña no coinciden"
+#comentario para hacer el push 
+#pide 7 datos Mail:str, Edad, Nombre_de_usuario, Apellido_de_usuario, Contrasenia, Genero, Ciudad
+#registra los datos junto con la fecha actual
+#devuelve un mensaje segun el resultado del registro
+@app.post("/Registro/")
+def Registrar_Datos_Usuario(user_date:Registrar_Datos_Usuario_class):
+
+    usuario_doc=db.collection('usuarios').where("Datos_registro.Mail","==",user_date.Mail).get()
+    
+    if(user_date.Edad<6 or user_date.Edad>100 ):
+        Mensaje="Edad invalida"
+    elif (not(user_date.Apellido_de_usuario.isalpha()) or not(user_date.Nombre_de_usuario.isalpha())):
+        Mensaje="Apellido o Nombre invalido"
+    elif usuario_doc!=[]:
+         Mensaje="Usuario existe"
     else:
-            
-        for clave in usuario_doc:
-            key=clave.id
-        usuario_doc_completo=db.collection('usuarios').document(key).get()
-        usuario_diccionario=usuario_doc_completo.to_dict()
-        if usuario_diccionario['contraseña']== user_date.contrasenia :
-
-            Mensaje="Usuario y/ contraseña ingresados correctamente"
-        else:
-            Mensaje="Contraseña ingresada incorrectamente"
-       
-    
-    return {"Mensaje":Mensaje}
-"""
+        datos={"Datos_registro":{"Mail": user_date.Mail,"Edad": user_date.Edad,"Apellido_de_usuario": user_date.Apellido_de_usuario,"Nombre_de_usuario": user_date.Nombre_de_usuario,"Contrasenia": user_date.Contrasenia,"Genero": user_date.Genero,"Ciudad": user_date.Ciudad,"Fecha de registro":datetime.now()}}
+        db.collection('usuarios').document().set(datos,merge=True)
+        Mensaje="Se registro correctamente"
+    return Mensaje
 if __name__ == "__main__":
-    
     uvicorn.run("index:app",reload=True)
