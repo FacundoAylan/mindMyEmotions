2c5aa68e834948ef3b9c176e9ed82724296a00c5
FacundoAylan <facundogsb@gmail.com>
2023-03-11 21:55:06 -0300

    Styling...

--- Client/Component/Home/home.js
+++ Client/Component/Home/home.js
@@ -1,21 +1,20 @@
-import React from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect} from "react";
+import { Image, StyleSheet, View } from "react-native";
 
 function Home ({navigation}) {
+  useEffect(()=> {
+    setTimeout(() => {
+      navigation.navigate("login")
+    }, 2000); 
+  },[])
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={{
-          uri: "https://i.ibb.co/Vw9v0p5/mind-My-Emotion.png",
-        }}
-      />
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("login")}
-      >
-        <Text style={styles.text}>Ingresar</Text>
-      </TouchableOpacity>
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://i.ibb.co/Vw9v0p5/mind-My-Emotion.png",
+          }}
+        />
     </View>
   );
 }
@@ -29,19 +28,7 @@ const styles = StyleSheet.create({
     borderLeftColor: '#f29100',
     borderRightWidth: 5,
     borderRightColor: '#662483',
-    minHeight: "100%",
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 120,
-    borderRadius: 12,
-    elevation: 3,
-    marginTop: 20,
-    borderWidth: 2,
-    borderColor: '#662483',
-    backgroundColor: 'white'
+    height: "100%",
   },
   text: {
     fontSize: 16,
@@ -49,7 +36,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     letterSpacing: 0.25,
     color: '#662483',
-    backgroundColor: 'none'
+    backgroundColor: 'none',
   },
   image:{
     width: '81%',
--- Client/Component/HomeMain/Choice/choice.js
+++ Client/Component/HomeMain/Choice/choice.js
@@ -1,15 +1,24 @@
-import React from "react";
-import { Alert, Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import {  Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Modal from "react-native-modal";
 
 export default function Choice({ navigation, route }) {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [random, setRandom] = useState();
   const { name, nameTheory, practice } = route.params;
   const json = practice.filter((value) => value.nameTheory === nameTheory);
+  const url = ['https://i.ibb.co/QmQc16N/1.png','https://i.ibb.co/tBRn87m/2.png','https://i.ibb.co/YRmhjkY/3.png', 'https://i.ibb.co/gJwJ3Xg/4.png']
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
   const validate = (answer) => {
-    return (json[0].answer === answer
-    ? navigation.navigate("topincs", {
-        name: name,
-      })
-    : Alert.alert('Respuesta incorrecta'))
+    if(practice[0].answer === answer){
+      return ( navigation.navigate("topincs", {name: name,}))
+    }else{
+      setRandom(Math.floor(Math.random() * 3));
+      toggleModal()
+    }
+    
   }
   return (
     <View style={styles.mainContainer}>
@@ -17,15 +26,28 @@ export default function Choice({ navigation, route }) {
       <Text style={styles.textContainer}>{json[0].text}</Text>
       {json[0].questions.map((value) => {
         return (
-          <TouchableOpacity
-            onPress={() => validate(value.charAt(0))}
-          >
+          <TouchableOpacity onPress={() => validate(value.charAt(0))}>
             <View style={styles.container}>
               <Text style={styles.textContainer}>{value}</Text>
             </View>
           </TouchableOpacity>
         );
       })}
+      <Modal isVisible={isModalVisible} style={{padding:0, margin:15}}>
+        <View >
+          <Image
+            style={styles.image}
+            source={{
+              uri: url[random],
+            }}
+          />
+            <TouchableOpacity onPress={() => setModalVisible(!isModalVisible)}>
+                <View style={styles.containerButton}>
+                  <Text style={styles.textContainer}>Intentar de nuevo</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -56,4 +78,23 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "purple",
   },
+  image:{
+    width: '100%',
+    height: '90%',
+    borderRadius: 12,
+    resizeMode: 'stretch'
+  },
+  containerButton:{
+    flex:0,
+    justifyContent: 'center',
+    textAlign: 'center',
+    height: 40,
+    width: '100%',
+    backgroundColor: 'white',
+    borderRadius:12,
+    fontSize: 30,
+    marginTop: 6,
+    borderColor: '#662483',
+    borderWidth:2
+  }
 });
