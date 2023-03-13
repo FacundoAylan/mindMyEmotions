2bf4bd2807f176e765b285c3f73afbfbe491256c
zeuspsy0x1 <zeusvargas26@gmail.com>
2023-03-12 20:25:52 -0500

    feat: profile progress with updated dependencies

--- Client/Component/Home/home.js
+++ Client/Component/Home/home.js
@@ -1,29 +1,29 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { Image, StyleSheet, View } from "react-native";
 import { useDispatch } from "react-redux";
 import { increment, loadValidate } from '../../redux/slice/index'
 
-function Home ({navigation}) {
+function Home( { navigation } ) {
   const dispatch = useDispatch();
-  useEffect(()=> {
-    dispatch(increment);
-    dispatch(loadValidate);
-    setTimeout(() => {
-      navigation.navigate("login")
-    }, 2000); 
-  },[])
+  useEffect( () => {
+    dispatch( increment );
+    dispatch( loadValidate );
+    setTimeout( () => {
+      navigation.navigate( "login" )
+    }, 2000 );
+  }, [] )
   return (
     <View style={styles.container}>
-        <Image
-          style={styles.image}
-          source={{
-            uri: "https://i.ibb.co/Vw9v0p5/mind-My-Emotion.png",
-          }}
-        />
+      <Image
+        style={styles.image}
+        source={{
+          uri: "https://i.ibb.co/Vw9v0p5/mind-My-Emotion.png",
+        }}
+      />
     </View>
   );
 }
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
   container: {
     flex: 1,
     backgroundColor: 'white',
@@ -41,13 +41,13 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     letterSpacing: 0.25,
     color: '#662483',
-    backgroundColor: 'none',
+    /* backgroundColor: 'none', */
   },
-  image:{
+  image: {
     width: '81%',
     height: 60,
     borderRadius: 12,
     resizeMode: 'stretch'
   }
-})
+} )
 export default Home;
